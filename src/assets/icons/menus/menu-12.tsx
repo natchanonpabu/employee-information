5c17e264404a7ced8b120a34fb711859ea2f635c
natchanon.pabu <natchanon.pabu@gmail.com>
2025-04-19 23:42:44 +0700

    feat(menu-icons): add menu icons

---
+++ src/assets/icons/menus/menu-12.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+
+export const Menu12 = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 22 19"
+    width={22}
+    height={19}
+    fill="none"
+    {...props}
+  >
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="m6 5 5 3 5-3"
+    />
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeWidth={1.5}
+      d="M12.5 17H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v8"
+    />
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="M15 15.5h6m0 0L18.5 13m2.5 2.5L18.5 18"
+    />
+  </svg>
+);
---
+++ src/assets/icons/menus/menu-13.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+
+export const Menu13 = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 22 22"
+    width={22}
+    height={22}
+    {...props}
+  >
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="M11 14a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+    />
+    <path
+      stroke="#fff"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={1.5}
+      d="m18.622 9.395-1.097-2.65L19 5l-2-2-1.735 1.483-2.707-1.113L11.935 1H9.981l-.632 2.401-2.645 1.115L5 3 3 5l1.453 1.789-1.08 2.657L1 10v2l2.401.656L4.516 15.3 3 17l2 2 1.791-1.46 2.606 1.072L10 21h2l.604-2.387 2.651-1.098C15.697 17.832 17 19 17 19l2-2-1.484-1.75 1.098-2.652 2.386-.62V10l-2.378-.605Z"
+    />
+  </svg>
+);
