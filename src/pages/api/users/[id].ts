c197c9b2e3f9c6ced4d34d20679b7dd062b60545
natchanon.pabu <natchanon.pabu@gmail.com>
2025-04-20 11:15:59 +0700

    test(mock-users): add mock users api

---
+++ src/pages/api/users/[id].ts
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { mockUsers } from "@/lib/mock-users";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    query: { id },
+    method,
+  } = req;
+
+  const index = mockUsers.findIndex((u) => u.id === id);
+
+  if (index === -1 && method !== "POST") {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  switch (method) {
+    case "GET":
+      return res.status(200).json(mockUsers[index]);
+
+    case "PATCH": {
+      mockUsers[index] = { ...mockUsers[index], ...req.body };
+      return res.status(200).json(mockUsers[index]);
+    }
+
+    case "DELETE": {
+      const deleted = mockUsers[index];
+      mockUsers.splice(index, 1);
+      return res.status(200).json({ message: "Deleted", user: deleted });
+    }
+
+    default:
+      res.setHeader("Allow", ["GET", "PUT", "DELETE"]);
+      return res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
---
+++ src/pages/api/users/index.ts
@@ -0,0 +1,44 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { mockUsers } from "@/lib/mock-users";
+import dayjs from "dayjs";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    const { employeeNo, name, registerDate } = req.query;
+
+    let filteredUsers = mockUsers;
+
+    if (name) {
+      filteredUsers = filteredUsers.filter((user) =>
+        user.name.toLowerCase().includes(String(name).toLowerCase())
+      );
+    }
+
+    if (employeeNo) {
+      filteredUsers = filteredUsers.filter((user) =>
+        user.name.toLowerCase().includes(String(employeeNo).toLowerCase())
+      );
+    }
+
+    if (registerDate) {
+      filteredUsers = filteredUsers.filter(
+        (user) => user.registerDate === String(registerDate)
+      );
+    }
+
+    return res.status(200).json(filteredUsers);
+  }
+
+  if (req.method === "POST") {
+    const newUser = {
+      id: dayjs().unix().toString(),
+      registerDate: dayjs().format("DD/MM/YYYY"),
+      ...req.body,
+    };
+    mockUsers.push(newUser);
+    return res.status(201).json(newUser);
+  }
+
+  res.setHeader("Allow", ["GET", "POST"]);
+  res.status(405).end(`Method ${req.method} Not Allowed`);
+}
